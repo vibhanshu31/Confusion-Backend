37c78a22aeb0a90ef6056d2ad8cbc5e8e4edcd11
Vibhanshu Shandil <vibhanshushandil31@gmail.com>
2020-06-19 13:20:44 +0530

    Passport

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 const mongoose = require('mongoose');
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
@@ -39,27 +41,21 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-  console.log(req.session);
-
-  if(!req.session.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
-  }
-  else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
+    if (!req.user) {
       var err = new Error('You are not authenticated!');
       err.status = 403;
-      return next(err);
+      next(err);
     }
-  }
+    else {
+          next();
+    }
 }
 
 app.use(auth);
